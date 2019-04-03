43414b514600d8d9ce7f5ff998cd7e1168185071
yingwuluohan <dfnwin@126.com>
2019-04-03 17:47:31 +0800

    p

--- web/src/main/java/com/unisound/iot/controller/jdk/JmapController.java
+++ web/src/main/java/com/unisound/iot/controller/jdk/JmapController.java
@@ -19,6 +19,10 @@ public class JmapController {
 
     @Autowired
     private ReportService reportService;
+    private int stackLength = 1;
+
+    private String memory = "public String getMsgHistory(HttpServletRequest request, @PathVariable(name=\"id\") long id)";
+
     //map/test/12
     @SuppressWarnings("unchecked")
     @RequestMapping(value="test/{id}",method = {RequestMethod.GET, RequestMethod.POST})
@@ -32,7 +36,7 @@ public class JmapController {
                 for( int i = 0 ; i < 1000000000; i ++ ){
                     List< String > list = new ArrayList<>();
                     list.add(i +"" );
-                    thread1 thread = new thread1( list , i + "test" );
+                    ThreadTest thread = new ThreadTest( list , i + "test" );
                     Thread threadGo = new Thread( thread );
                     threadGo.start();
                     System.out.println( "for循环：" + i );
@@ -47,20 +51,64 @@ public class JmapController {
         return "ok";
     }
 
-    class thread1 implements Runnable{
+    /**
+     * 堆栈溢出
+     * map/test/stack
+     */
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value="test/stack",method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public void makeStackOverflowError(){
+        stackLength++;
+        makeStackOverflowError();
+    }
 
-        public thread1(List list, String content ){
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value="test/out",method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public void makeOutofMemoryError(){
+
+        try{
+            for( int i = 0 ;i < 10 ;i++ ){
+                List<String > list = new ArrayList<>();
+                ThreadTest test = new ThreadTest( list , memory );
+                Thread thread = new Thread( test );
+                thread.start();
+            }
+        }catch ( Exception e ){
+            System.out.println( "抛异常："  +e.getMessage()  );
+            e.printStackTrace();
+        }
+
+
+    }
+
+    class ThreadTest implements Runnable{
+
+        public ThreadTest(List<String> list, String content ){
             this.list = list;
             this.content = content;
         }
 
-        private List list;
+        private List<String> list;
         private String content;
 
 
         public void run(){
-            list.add( content );
-            System.out.println( "list 大小：" + list.size() );
+//          List<String > list = new ArrayList<>();
+            try{
+                System.out.println(  "当前线程：" + Thread.currentThread().getName() );
+                while( true ){
+
+                    list.add(  memory+"dddddd" + Math.random() );
+
+                    System.out.println( Thread.currentThread().getName()+": " + list.size() );
+                }
+            }catch ( Exception e ){
+                System.out.println( "抛异常：" + list.size() );
+                e.printStackTrace();
+            }
         }
 
 
