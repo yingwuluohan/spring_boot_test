f871e51ddaff0c0491a64fcf7e0876dda4be0ec0
yingwuluohan <dfnwin@126.com>
2019-04-09 11:38:12 +0800

    add zk

---
+++ web/src/main/java/com/unisound/iot/common/config/zookeeper/ZkConfiguration.java
@@ -0,0 +1,37 @@
+package com.unisound.iot.common.config.zookeeper;
+
+/**
+ * @Created by yingwuluohan on 2019/4/9.
+ * @Company 北京云知声技术有限公司
+ */
+
+import com.unisound.iot.common.zookeeper.ZkClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ZkConfiguration {
+    @Value("${zookeeper.server}")
+    private String zookeeperServer;
+    @Value(("${zookeeper.sessionTimeoutMs}"))
+    private int sessionTimeoutMs;
+    @Value("${zookeeper.connectionTimeoutMs}")
+    private int connectionTimeoutMs;
+    @Value("${zookeeper.maxRetries}")
+    private int maxRetries;
+    @Value("${zookeeper.baseSleepTimeMs}")
+    private int baseSleepTimeMs;
+
+    @Bean(initMethod = "init", destroyMethod = "stop")
+    public ZkClient zkClient() {
+        ZkClient zkClient = new ZkClient();
+        zkClient.setZookeeperServer(zookeeperServer);
+        zkClient.setSessionTimeoutMs(sessionTimeoutMs);
+        zkClient.setConnectionTimeoutMs(connectionTimeoutMs);
+        zkClient.setMaxRetries(maxRetries);
+        zkClient.setBaseSleepTimeMs(baseSleepTimeMs);
+        return zkClient;
+    }
+
+}
---
+++ web/src/main/java/com/unisound/iot/common/zookeeper/ZkClient.java
@@ -0,0 +1,109 @@
+package com.unisound.iot.common.zookeeper;
+
+/**
+ * @Created by yingwuluohan on 2019/4/9.
+ * @Company 北京云知声技术有限公司
+ */
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ZkClient {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private CuratorFramework client;
+    private String zookeeperServer;
+    private int sessionTimeoutMs;
+    private int connectionTimeoutMs;
+    private int baseSleepTimeMs;
+    private int maxRetries;
+
+    public void setZookeeperServer(String zookeeperServer) {
+        this.zookeeperServer = zookeeperServer;
+    }
+    public String getZookeeperServer() {
+        return zookeeperServer;
+    }
+    public void setSessionTimeoutMs(int sessionTimeoutMs) {
+        this.sessionTimeoutMs = sessionTimeoutMs;
+    }
+    public int getSessionTimeoutMs() {
+        return sessionTimeoutMs;
+    }
+    public void setConnectionTimeoutMs(int connectionTimeoutMs) {
+        this.connectionTimeoutMs = connectionTimeoutMs;
+    }
+    public int getConnectionTimeoutMs() {
+        return connectionTimeoutMs;
+    }
+    public void setBaseSleepTimeMs(int baseSleepTimeMs) {
+        this.baseSleepTimeMs = baseSleepTimeMs;
+    }
+    public int getBaseSleepTimeMs() {
+        return baseSleepTimeMs;
+    }
+    public void setMaxRetries(int maxRetries) {
+        this.maxRetries = maxRetries;
+    }
+    public int getMaxRetries() {
+        return maxRetries;
+    }
+
+    public void init() {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(baseSleepTimeMs, maxRetries);
+        client = CuratorFrameworkFactory.builder().connectString(zookeeperServer).retryPolicy(retryPolicy)
+                .sessionTimeoutMs(sessionTimeoutMs).connectionTimeoutMs(connectionTimeoutMs).build();
+        client.start();
+    }
+
+    public void stop() {
+        client.close();
+    }
+
+    public CuratorFramework getClient() {
+        return client;
+    }
+
+    public void register() {
+        try {
+            String rootPath = "/" + "services";
+            String hostAddress = InetAddress.getLocalHost().getHostAddress();
+            String serviceInstance = "prometheus" + "-" +  hostAddress + "-";
+            client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(rootPath + "/" + serviceInstance);
+        } catch (Exception e) {
+            logger.error("注册出错", e);
+        }
+    }
+
+    public List<String> getChildren(String path) {
+        List<String> childrenList = new ArrayList<>();
+        try {
+            childrenList = client.getChildren().forPath(path);
+        } catch (Exception e) {
+            logger.error("获取子节点出错", e);
+        }
+        return childrenList;
+    }
+
+    public int getChildrenCount(String path) {
+        return getChildren(path).size();
+    }
+
+    public List<String> getInstances() {
+        return getChildren("/services");
+    }
+
+    public int getInstancesCount() {
+        return getInstances().size();
+    }
+}
