3a4c33bababc039b0ed0c6299d78fd626e439d3f
yingwuluohan <dfnwin@126.com>
2019-05-28 18:36:24 +0800

    socket

---
+++ web/src/main/java/com/unisound/iot/controller/jdk/OutOfMemery/Metaspaces.java
@@ -0,0 +1,47 @@
+package com.unisound.iot.controller.jdk.OutOfMemery;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Exception in thread "main" java.lang.OutOfMemoryError: Compressed class space
+ * @Created by yingwuluohan on 2019/5/23.
+ * @Company 北京云知声技术有限公司
+ *
+ *
+ *
+ */
+public class Metaspaces extends ClassLoader {
+    public static void main(String[] args) {
+        // 类持有
+        List<Class<?>> classes = new ArrayList<Class<?>>();
+        // 循环1000w次生成1000w个不同的类。
+        for (int i = 0; i < 10000000; ++i) {
+            ClassWriter cw = new ClassWriter(0);
+            // 定义一个类名称为Class{i}，它的访问域为public，父类为java.lang.Object，不实现任何接口
+            cw.visit(Opcodes.V1_1, Opcodes.ACC_PUBLIC, "Class" + i, null,
+                    "java/lang/Object", null);
+            // 定义构造函数<init>方法
+            MethodVisitor mw = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>",
+                    "()V", null, null);
+            // 第一个指令为加载this
+            mw.visitVarInsn(Opcodes.ALOAD, 0);
+            // 第二个指令为调用父类Object的构造函数
+            mw.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V" );
+            // 第三条指令为return
+            mw.visitInsn(Opcodes.RETURN);
+            mw.visitMaxs(1, 1);
+            mw.visitEnd();
+
+            Metaspaces test = new Metaspaces();
+            byte[] code = cw.toByteArray();
+            // 定义类
+            Class<?> exampleClass = test.defineClass("Class" + i, code, 0, code.length);
+            classes.add(exampleClass);
+        }
+    }
+}
