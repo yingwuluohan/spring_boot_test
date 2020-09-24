ec7c212c618be75b9e575d34444894a5a5163bdc
dongfangnan <dongfangnan@gokuaidian.com>
2020-09-24 14:21:59 +0800

    up

---
+++ web/src/main/java/com/unisound/iot/controller/jdk/integer/IntegerCache.java
@@ -0,0 +1,30 @@
+package com.unisound.iot.controller.jdk.integer;
+
+import java.lang.reflect.Field;
+
+public class IntegerCache {
+
+
+
+
+
+
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+
+        Class cache = Integer.class.getDeclaredClasses()[0]; //1
+        Field myCache = cache.getDeclaredField("cache"); //2
+        myCache.setAccessible(true);//3
+
+        Integer[] newCache = (Integer[]) myCache.get(cache); //4
+        newCache[132] = newCache[133]; //5
+
+        int a = 2;
+        int b = a + a;
+        System.out.printf("%d + %d = %d", a, a, b); //
+    }
+
+
+
+
+}
--- web/src/main/resources/publishJar.sh
+++ web/src/main/resources/publishJar.sh
@@ -1,71 +1,52 @@
 #!/bin/bash
-#use default JAVA_HOME config
-#export JAVA_HOME=/usr/java/jdk1.8.0_51
-#export PATH=$PATH:$JAVA_HOME/bin
-
-#defined
-PROJECT_HOME="/data/war"
-TOMCAT_HOME="/data/servers/tomcat-21120-user"
-TOMCAT_PORT=9001
-PROJECT="$1"
-stop()
-{
-  if [ -f $TOMCAT_HOME/bin/shutdown.sh ];
-        then
-          echo $"Stopping Tomcat"
-          $TOMCAT_HOME/bin/shutdown.sh
-          RETVAL=$?
-          sleep 1
-          ps -ef|grep $TOMCAT_PORT|egrep -v 'grep|init|publish|tail'|awk '{print$2}'|xargs kill -9
-          echo " OK"
-          # [ $RETVAL -eq 0 ] && rm -f /var/lock/...
-          return $RETVAL
-      fi
-}
-
-#param validate
-if [ $# -lt 1 ]; then
-  echo "you must use like this : ./publish.sh <projectname> [tomcat port] [tomcat home dir]"
-  exit
+if [ $# -lt 2 ]
+then
+  echo "you must use like this : ./publishJar.sh <projectname> <port> <active>"
+  exit 1
 fi
-if [ "$2" != "" ]; then
-   TOMCAT_PORT=$2
-fi
-if [ "$3" != "" ]; then
-   TOMCAT_HOME="$3"
+SERVER_NAME="$1"
+PORT=$2
+ACTIVE=$3
+BASE_PATH="/data/servers/${SERVER_NAME}"
+
+OPTS="--spring.application.name=${SERVER_NAME} --server.port=${PORT}"
+if [ -n "$ACTIVE" ]
+then
+  OPTS="$OPTS --spring.profiles.active=${ACTIVE}"
 fi
 
-#shutdown tomcat
-stop
-sleep 1
+BAK_DIR="/data/publish/backup_data/$SERVER_NAME"
+if [ ! -d $BAK_DIR ]
+then
+  echo "create bak dir: $BAK_DIR"
+  mkdir -p $BAK_DIR
+  mkdir -p "/data/logs/${SERVER_NAME}"
+fi
 
-#bak project
-sourceFile="$PROJECT_HOME"/$PROJECT.war
-if [ -f $sourceFile ]; then
-  echo "source file exist, start backup"
-  BAK_DIR=/data/publish/backup_data/$PROJECT/`date +%Y%m%d`
-  mkdir -p "$BAK_DIR"
-  backupFile="$BAK_DIR"/"$PROJECT"_`date +%H%M%S`.war
-  cp $sourceFile $backupFile
-  echo "backup file : $backupFile"
-  echo "backup file [OK]"
+HAS_OLD_JAR=`ls $BASE_PATH | grep -c ${SERVER_NAME}-[0-9].**.jar`
+if [ $HAS_OLD_JAR -gt 0 ]
+then
+  echo "backup old jar $HAS_OLD_JAR"
+  mv -f ${BASE_PATH}/${SERVER_NAME}-[0-9].**.jar $BAK_DIR/${SERVER_NAME}_`date +%H%M%S`.jar
+fi
+
+if [ ! -d $BASE_PATH ]
+then
+  echo "create server dir: $BASE_PATH"
+  mkdir -p $BASE_PATH
+fi
+
+mv -f /data/publish/data/${SERVER_NAME}-**.jar $BASE_PATH
+JAR_FILE=`find ${BASE_PATH}/${SERVER_NAME}-**.jar`
+if [ -f "$JAR_FILE" ]
+then
+  chmod 777 $JAR_FILE
+  echo deploying $JAR_FILE
+  ps -ef | grep $SERVER_NAME-[0-9].**.jar | grep -v grep | awk '{print $2}' | xargs kill -9
+  echo "start server $JAR_FILE"
+  nohup java -javaagent:/data/apm/elastic-apm-agent-1.17.0.jar -Delastic.apm.enable_log_correlation=true -Delastic.apm.environment=$ACTIVE -Delastic.apm.service_name=$SERVER_NAME -Delastic.apm.server_url=http://uwork-apm.gokuaidian.com -Delastic.apm.application_packages=com.fp.*,tech.uwork.*,com.xxl.job.*,com.gkd.*,com.czb.*,com.gokuaidian.* -jar -Xms1024m -Xmx1024m -Djava.security.egd=file:/dev/./urandom $JAR_FILE $OPTS > /data/logs/${SERVER_NAME}/console.log 2>&1&
+  echo "$JAR_FILE published"
 else
-  echo "$sourceFile not exist, skip backup"
+  echo "error : $JAR_FILE not exists"
+  exit 1
 fi
-
-#publish project
-echo "$PROJECT is publishing"
-rm -rf "$PROJECT_HOME"/$PROJECT/*
-rm -rf "$PROJECT_HOME"/$PROJECT.war
-cp /data/publish/data/$PROJECT.war "$PROJECT_HOME"/$PROJECT.war
-echo "publish project [OK]"
-
-#remove tmp
-rm -rf /data/publish/data/$PROJECT.war
-echo "remove temp file: /czb/publish/data/$PROJECT.war [OK]"
-rm -rf $TOMCAT_HOME/work/*
-echo "remove temp file: $TOMCAT_HOME/work/* [OK]"
-
-#start tomcat
-#"$TOMCAT_HOME"/bin/startup.sh
-#echo "tomcat is starting [OK]"
