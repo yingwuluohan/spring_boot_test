487514219247992ac113814518b3c1e1b5b57b2d
fangnan <dongfangnan@gokuaidian.com>
2021-04-09 15:59:15 +0800

    d

--- web/src/main/java/com/unisound/iot/controller/elasticSearchUtil/condition/Test.java
+++ web/src/main/java/com/unisound/iot/controller/elasticSearchUtil/condition/Test.java
@@ -5,16 +5,16 @@ public class Test {
 
     public void operater(){
 //        //TODO test es
-////        List<DocumentCommandRequest> documentCommandRequestList = Lists.newArrayListWithCapacity(1);
-////        DataSyncEsDTO dataSyncEsDTO = null ;
-////        DocumentCommandRequest documentCommandRequest = DocumentCommandRequest.of(DocumentCommandType.UPDATE, "data", "data", dataId).setDocument(dataSyncEsDTO);
-////        documentCommandRequestList.add(documentCommandRequest);
-////        BulkRequestBuilder bulkRequestBuilder = elasticSearchClient.prepareBulk();
-////        UpdateRequestBuilder updateRequestBuilder = elasticSearchClient.prepareUpdate(documentCommandRequest.getIndex(), documentCommandRequest.getType(), documentCommandRequest.getId()).setDoc(objectMapper.writeValueAsBytes(documentCommandRequest.getDocument()));
-////        if(documentCommandRequest.getParent() != null){
-////            updateRequestBuilder.setParent(documentCommandRequest.getParent());
-////        }
-////        BulkResponse bulkResponse = bulkRequestBuilder.get();
+//        List<DocumentCommandRequest> documentCommandRequestList = Lists.newArrayListWithCapacity(1);
+//        DataSyncEsDTO dataSyncEsDTO = null ;
+//        DocumentCommandRequest documentCommandRequest = DocumentCommandRequest.of(DocumentCommandType.UPDATE, "data", "data", dataId).setDocument(dataSyncEsDTO);
+//        documentCommandRequestList.add(documentCommandRequest);
+//        BulkRequestBuilder bulkRequestBuilder = elasticSearchClient.prepareBulk();
+//        UpdateRequestBuilder updateRequestBuilder = elasticSearchClient.prepareUpdate(documentCommandRequest.getIndex(), documentCommandRequest.getType(), documentCommandRequest.getId()).setDoc(objectMapper.writeValueAsBytes(documentCommandRequest.getDocument()));
+//        if(documentCommandRequest.getParent() != null){
+//            updateRequestBuilder.setParent(documentCommandRequest.getParent());
+//        }
+//        BulkResponse bulkResponse = bulkRequestBuilder.get();
 
     }
 
